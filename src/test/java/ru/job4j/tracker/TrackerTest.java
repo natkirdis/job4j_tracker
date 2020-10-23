5df16233257541ca9afb0e37cb9a3e680d91e4de
Natalia Kirdis <kirdisnatalia@gmail.com>
2020-10-23 17:52:22 +0300

    Input и полиморфизм.[#188771]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -6,32 +6,13 @@ import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
     @Test
-    public void whenAddNewItemThenTrackerHasSameItem() {
+    public void whenAddItem() {
+        String[] answers = {"Fix PC"};
+        Input input = new StubInput(answers);
         Tracker tracker = new Tracker();
-        Item item = new Item("test1");
-        tracker.add(item);
-        Item result = tracker.findById(item.getId());
-        assertThat(result.getName(), is(item.getName()));
-    }
-
-    @Test
-    public void whenFindItemByName() {
-        Tracker tracker = new Tracker();
-        Item item = new Item("test1");
-        tracker.add(item);
-        Item[] result = tracker.findByName("test1");
-        System.out.println(result[0].getName());
-        assertThat(result[0].getName(), is("test1"));
-    }
-
-    @Test
-    public void whenFindAll() {
-        Tracker tracker = new Tracker();
-        Item item1 = new Item("test1");
-        Item item2 = new Item("test2");
-        tracker.add(item1);
-        tracker.add(item2);
-        Item[] result = tracker.findAll();
-        assertThat(result[0].getName(), is("test1"));
+        StartUI.createItem(input, tracker);
+        Item created = tracker.findAll()[0];
+        Item expected = new Item("Fix PC");
+        assertThat(created.getName(), is(expected.getName()));
     }
 }
